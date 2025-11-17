fbd483c1b639a00c0bd656fed381d24cda25f1c9
Erikgavs <erikgavs@gmail.com>
2025-11-17 22:17:59 +0000

    v3

--- reporter.py
+++ reporter.py
@@ -27,12 +27,21 @@ def enviar_mensaje():
 
     # Meterle ps aux
 
-    # SYS INFO
+    # SYS INFO (psutil)
     porcentaje_mem = psutil.cpu_percent(interval=1)
     memoria = psutil.virtual_memory()
     memoria_usada = memoria.used / 1024**2 # Conversión de Bytes a MB
     disco = psutil.disk_usage("/")
     disco_usado = disco.used / 1024**2 # Conversión de Bytes a MB
+    temperatura = psutil.sensors_temperatures()
+    if temperatura:
+        primer_sensor = list(temperatura.values())[0][0] # La lista de sensores se convierte en un array y seleccionamos el primer valor dela array
+        # se van a hacer arrays por sensor y dentro de los sensores hay elementos, seleccionamos el primer elemento del primer sensor
+        temperatura_actual = primer_sensor.current
+        temperatura_max = primer_sensor.hight
+        temperatura_crit = primer_sensor.critical
+    else:
+        temperatura_actual = temperatura_max = temperatura_crit = None
 
     # :.2f => Aceptamos 2 decimales
     info =f"""
@@ -42,6 +51,11 @@ def enviar_mensaje():
     Uso de CPU: {porcentaje_mem:.2f}%
     Memoria: {memoria_usada:.2f} Mb
     Uso del Disco: {disco_usado:.2f} Mb
+
+    Temperatura
+    Temperatura actual: {f"{temperatura_actual:.2f} ºC" if temperatura_actual else "No disponible"}
+    Temperatura máxima recomendada: {f"{temperatura_max:.2f} ºC" if temperatura_max else "No disponible"}
+    Temperatura crítica: {f"{temperatura_crit:.2f} ºC" if temperatura_crit else "No disponible"}
 
     Ping al server
     {ping_server_output}
